2c4f1426e0451d994bb580b3d495a6f095e3d88d
izhx <izhx404@gmail.com>
2020-03-19 15:15:08 +0800

    使用allen的chuliu算法

--- tjunlp/models/dependency_parser.py
+++ tjunlp/models/dependency_parser.py
@@ -3,10 +3,9 @@
 """
 
 from typing import Dict, List, Any
-from collections import defaultdict, OrderedDict
+from collections import OrderedDict
 import math
 
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -18,170 +17,13 @@ from ..modules.fusion import Fusion
 from ..modules.dropout import WordDropout
 from ..modules.linear import NonLinear, Bilinear
 from ..modules.util import initial_parameter
+from ..nn.chu_liu_edmonds import batch_decode_head
 
 
 def remove_sep(tensors: List[torch.Tensor]):
     for i in range(len(tensors)):
         tensors[i] = tensors[i][:, :-1]
     return tensors
-
-
-def _mst(scores):
-    """
-    with some modification to support parser output for MST decoding
-    https://github.com/tdozat/Parser/blob/0739216129cd39d69997d28cbc4133b360ea3934/lib/models/nn.py#L692
-    """
-    length = scores.shape[0]
-    min_score = scores.min() - 1
-    eye = np.eye(length)
-    scores = scores * (1 - eye) + min_score * eye
-    heads = np.argmax(scores, axis=1)
-    heads[0] = 0
-    tokens = np.arange(1, length)
-    roots = np.where(heads[tokens] == 0)[0] + 1
-    if len(roots) < 1:
-        root_scores = scores[tokens, 0]
-        head_scores = scores[tokens, heads[tokens]]
-        new_root = tokens[np.argmax(root_scores / head_scores)]
-        heads[new_root] = 0
-    elif len(roots) > 1:
-        root_scores = scores[roots, 0]
-        scores[roots, 0] = 0
-        new_heads = np.argmax(scores[roots][:, tokens], axis=1) + 1
-        new_root = roots[np.argmin(
-            scores[roots, new_heads] / root_scores)]
-        heads[roots] = new_heads
-        heads[new_root] = 0
-
-    edges = defaultdict(set)
-    vertices = {0}
-    for dep, head in enumerate(heads[tokens]):
-        vertices.add(dep + 1)
-        edges[head].add(dep + 1)
-    for cycle in _find_cycle(vertices, edges):
-        dependents = set()
-        to_visit = set(cycle)
-        while len(to_visit) > 0:
-            node = to_visit.pop()
-            if node not in dependents:
-                dependents.add(node)
-                to_visit.update(edges[node])
-        cycle = np.array(list(cycle))
-        old_heads = heads[cycle]
-        old_scores = scores[cycle, old_heads]
-        non_heads = np.array(list(dependents))
-        scores[np.repeat(cycle, len(non_heads)),
-               np.repeat([non_heads], len(cycle), axis=0).flatten()] = min_score
-        new_heads = np.argmax(scores[cycle][:, tokens], axis=1) + 1
-        new_scores = scores[cycle, new_heads] / old_scores
-        change = np.argmax(new_scores)
-        changed_cycle = cycle[change]
-        old_head = old_heads[change]
-        new_head = new_heads[change]
-        heads[changed_cycle] = new_head
-        edges[new_head].add(changed_cycle)
-        edges[old_head].remove(changed_cycle)
-
-    return heads
-
-
-def _find_cycle(vertices, edges):
-    """
-    https://en.wikipedia.org/wiki/Tarjan%27s_strongly_connected_components_algorithm
-    https://github.com/tdozat/Parser/blob/0739216129cd39d69997d28cbc4133b360ea3934/lib/etc/tarjan.py
-    """
-    _index = 0
-    _stack = []
-    _indices = {}
-    _lowlinks = {}
-    _onstack = defaultdict(lambda: False)
-    _SCCs = []  # pylint:disable=invalid-name
-
-    def _strongconnect(v):  # pylint:disable=invalid-name
-        nonlocal _index
-        _indices[v] = _index
-        _lowlinks[v] = _index
-        _index += 1
-        _stack.append(v)
-        _onstack[v] = True
-
-        for w in edges[v]:  # pylint:disable=invalid-name
-            if w not in _indices:
-                _strongconnect(w)
-                _lowlinks[v] = min(_lowlinks[v], _lowlinks[w])
-            elif _onstack[w]:
-                _lowlinks[v] = min(_lowlinks[v], _indices[w])
-
-        if _lowlinks[v] == _indices[v]:
-            SCC = set()  # pylint:disable=invalid-name
-            while True:
-                w = _stack.pop()  # pylint:disable=invalid-name
-                _onstack[w] = False
-                SCC.add(w)
-                if not w != v:
-                    break
-            _SCCs.append(SCC)
-
-    for vertex in vertices:
-        if vertex not in _indices:
-            _strongconnect(vertex)
-
-    return [SCC for SCC in _SCCs if len(SCC) > 1]
-
-
-class GraphParser(object):
-    """
-    基于图的parser base class, 支持贪婪解码和最大生成树解码
-    """
-
-    @staticmethod
-    def greedy_decoder(arc_matrix, mask=None):
-        """
-        贪心解码方式, 输入图, 输出贪心解码的parsing结果, 不保证合法的构成树
-        :param arc_matrix: [batch, seq_len, seq_len] 输入图矩阵
-        :param mask: [batch, seq_len] 输入图的padding mask, 有内容的部分为 1, 否则为 0.
-            若为 ``None`` 时, 默认为全1向量. Default: ``None``
-        :return heads: [batch, seq_len] 每个元素在树中对应的head(parent)预测结果
-        """
-        _, seq_len, _ = arc_matrix.shape
-        matrix = arc_matrix + torch.diag(arc_matrix.new(seq_len).fill_(-np.inf))
-        flip_mask = mask.eq(False)
-        matrix.masked_fill_(flip_mask.unsqueeze(1), -np.inf)
-        _, heads = torch.max(matrix, dim=2)
-        if mask is not None:
-            heads *= mask.long()
-        return heads
-
-    @staticmethod
-    def mst_decoder(arc_matrix, mask=None):
-        """
-        用最大生成树算法, 计算parsing结果, 保证输出合法的树结构
-        :param arc_matrix: [batch, seq_len, seq_len] 输入图矩阵
-        :param mask: [batch, seq_len] 输入图的padding mask, 有内容的部分为 1, 否则为 0.
-            若为 ``None`` 时, 默认为全1向量. Default: ``None``
-        :return heads: [batch, seq_len] 每个元素在树中对应的head(parent)预测结果
-        """
-        batch_size, seq_len, _ = arc_matrix.shape
-        matrix = arc_matrix.clone()
-        ans = matrix.new_zeros(batch_size, seq_len).long()
-        lens = (mask.long()).sum(
-            1) if mask is not None else torch.zeros(batch_size) + seq_len
-        for i, graph in enumerate(matrix):
-            len_i = lens[i]
-            ans[i, :len_i] = torch.as_tensor(
-                _mst(graph.detach()[:len_i, :len_i].cpu().numpy()), device=ans.device)
-        if mask is not None:
-            ans *= mask.long()
-        return ans
-
-    @staticmethod
-    def decode(graph: torch.Tensor, mask: torch.Tensor, greedy: bool = False):
-        # if greedy:
-        #     graph = GraphParser.greedy_decoder(graph, mask)
-        # else:
-        #     graph = GraphParser.mst_decoder(graph, mask)
-        graph = graph.max(2)[1]
-        return graph
 
 
 class GELU(nn.Module):
@@ -193,7 +35,7 @@ class GELU(nn.Module):
         return 0.5 * x * (1 + torch.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * torch.pow(x, 3))))
 
 
-class DependencyParser(Model, GraphParser):
+class DependencyParser(Model):
     """
     主Parser，可更换不同的embedding和encoder。
     """
@@ -209,7 +51,7 @@ class DependencyParser(Model, GraphParser):
                  arc_dim: int = 250,
                  label_dim: int = 50,
                  dropout: float = 0,
-                 greedy_infer: bool = False,
+                 greedy_infer: bool = True,
                  **kwargs):
         super().__init__(criterion)
         self.word_embedding = build_word_embedding(
@@ -267,7 +109,10 @@ class DependencyParser(Model, GraphParser):
 
         self.arc_classifier = Bilinear(arc_dim, arc_dim, 1)
         self.rel_classifier = Bilinear(label_dim, label_dim, len(vocab['deprel']))
-        self.decoder = lambda x, y: self.decode(x, y, greedy_infer)
+        if greedy_infer:
+            self.decoder = lambda x, y: x.max(2)[1]
+        else:
+            self.decoder = batch_decode_head
         self.split_sizes = [arc_dim, label_dim]
         # for calculate metrics precisely
         self.metric_counter = OrderedDict({'arc': 0, 'rel': 0, 'num': 0})
@@ -278,6 +123,7 @@ class DependencyParser(Model, GraphParser):
                 mask: torch.Tensor,  # 有词的地方为True
                 head: torch.Tensor = None,
                 deprel: torch.Tensor = None,
+                seq_lens: torch.Tensor = None,
                 **kwargs) -> Dict[str, Any]:
         feat = self.word_embedding(words, **kwargs)
         feat = self.word_transform(feat)
@@ -289,7 +135,7 @@ class DependencyParser(Model, GraphParser):
 
         feat = self.word_dropout(feat)
         if self.encoder is not None:
-            feat = self.encoder(feat, **kwargs)  # unpack会去掉[SEP]那一列
+            feat = self.encoder(feat, seq_lens)  # unpack会去掉[SEP]那一列
             if feat.shape[1] == words.shape[1] - 1:
                 mask, head, deprel = remove_sep([mask, head, deprel])
 
@@ -305,7 +151,7 @@ class DependencyParser(Model, GraphParser):
         rel_pred = self.rel_classifier(feat[1], feat[3])  # (b,s,s,c)
 
         # use gold or predicted arc to predict label
-        head_pred = head if self.training else self.decoder(arc_pred, mask)
+        head_pred = head if self.training else self.decoder(arc_pred, seq_lens)
 
         rel_pred = torch.gather(rel_pred, 2, head_pred.unsqueeze(
             2).unsqueeze(3).expand(-1, -1, -1, rel_pred.shape[-1])).squeeze(2)
@@ -350,8 +196,7 @@ class DependencyParser(Model, GraphParser):
         mask = mask.long()
         mask[:, 0] = 0  # mask out <root> tag
         head_pred_correct = (head_pred == head_gt).long() * mask
-        rel_pred_correct = (
-                                   rel_pred == rel_gt).long() * head_pred_correct
+        rel_pred_correct = (rel_pred == rel_gt).long() * head_pred_correct
         arc = head_pred_correct.sum().item()
         rel = rel_pred_correct.sum().item()
         num = mask.sum().item()
